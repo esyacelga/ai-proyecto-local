6f050a6ac8e70ab21f3f0bc399b95e0ad7530a84
Santiago Yacelga <esyacelga@hotmail.com>
2024-05-03 13:55:54 -0500

    Docker Configurations

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from regresion_lineal_simple.src.notebooks.classes import salary_model 
 from .app.views import get_prediction
+from .app.regresion_lineal_simple import salary_model 
+ 
 
 
 
@@ -9,4 +10,4 @@ app = FastAPI(docs_url='/')
 
 @app.post('/v1/salaryPrediction')
 def salary_prediction_model_prediction(request: salary_model.SalaryPredictionRequest):
-    return salary_model.SalaryPredictionResponse(Salary=get_prediction(request,'regresion_lineal_simple/src/notebooks/model/model.pkl'))
+    return salary_model.SalaryPredictionResponse(Salary=get_prediction(request,'regresion_lineal_simple/src/notebooks/model/model-salary.pkl'))
