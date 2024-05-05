cca9b34afdc7ca942893d49a0697682f28b91bfb
Santiago Yacelga <esyacelga@hotmail.com>
2024-05-05 15:06:54 -0500

    Actualizacion de modelo

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from .app.views import get_prediction
 from .app.regresion_lineal_simple import salary_model 
+from .app.classes.regresion_lineal_multiple import profit_classes
  
 
 
@@ -11,3 +12,8 @@ app = FastAPI(docs_url='/')
 @app.post('/v1/salaryPrediction')
 def salary_prediction_model_prediction(request: salary_model.SalaryPredictionRequest):
     return salary_model.SalaryPredictionResponse(Salary=get_prediction(request,'regresion_lineal_simple/src/notebooks/model/model-salary.pkl'))
+
+
+@app.post('/v1/profitPrediction')
+def profit_prediction_model_prediction(request: profit_classes.ProfitRequest):
+    return profit_classes.ProfitResponse(Salary=get_prediction(request,'regresion_lineal_multiple/src/notebooks/model/model-profit.pkl'))
