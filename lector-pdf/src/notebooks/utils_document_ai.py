6dafb4d4c3b61c44560a7aeb422861fa1a7d3464
Santiago Yacelga <esyacelga@hotmail.com>
2024-06-02 21:03:57 -0500

    Pruebas

--- lector-pdf/src/notebooks/utils_document_ai.py
+++ lector-pdf/src/notebooks/utils_document_ai.py
@@ -1,6 +1,7 @@
 import os
 from google.cloud import storage
 from google.cloud import documentai_v1 as documentai
+import json
 
 # Configurar la ruta a tu archivo de clave JSON de Google Cloud
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/eyacelga/repositorio-codigo/ai-proyecto-local/lector-pdf/src/data/documental-isspol-664f93e7c2fb.json"
@@ -34,5 +35,36 @@ def leer_pdf_como_bytes(file_path):
         contenido_en_bytes = file.read()
     return contenido_en_bytes
 
+def buscar_objetos_por_nombre(json_data, nombre_objeto):
+    """
+    Busca recursivamente en el JSON y devuelve una lista de objetos que coinciden con el nombre del objeto.
+
+    Parámetros:
+    json_data (dict): Diccionario que representa el objeto JSON.
+    nombre_objeto (str): El nombre del objeto que se está buscando.
+
+    Retorna:
+    list: Lista de objetos encontrados que coinciden con el nombre del objeto.
+    """
+    resultados = []
+
+    def buscar_recursivamente(data):
+        if isinstance(data, dict):
+            for key, value in data.items():
+                if key == nombre_objeto:
+                    resultados.append(value)
+                if isinstance(value, (dict, list)):
+                    buscar_recursivamente(value)
+        elif isinstance(data, list):
+            for item in data:
+                if isinstance(item, (dict, list)):
+                    buscar_recursivamente(item)
+
+    buscar_recursivamente(json_data)
+    return resultados
+
 def procesar_documento(file_path):
     return onlineProcessing(location,project_id,file_path )
+
+def decirHola():
+    print("**************")
