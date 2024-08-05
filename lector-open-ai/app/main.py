b01406b3a2c7663daf21fd7a2976a21bed8843a1
Santiago Yacelga <esyacelga@hotmail.com>
2024-08-04 21:10:06 -0500

    .

--- lector-open-ai/app/main.py
+++ lector-open-ai/app/main.py
@@ -1,9 +1,21 @@
 
 from fastapi import FastAPI
 from app.adapters.web.endpoints import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origins = [
+    "http://localhost:4200",  # URL de tu aplicación Angular
+    "http://your-angular-app-domain.com",  # Agrega otras URLs según sea necesario
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(api_router, prefix="/api")
 
 # Opcional: Personaliza la documentación
